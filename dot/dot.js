c85c998bce041c18720e8068b0dcab6a38edf78c
Vladimir Zarypov <krre31@gmail.com>
2016-02-13 18:46:20 +0200

    removed \n signs from shader sources

--- dot/dot.js
+++ dot/dot.js
@@ -1,12 +1,10 @@
-var vertShaderSource = " \
-void main() { \n
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0); \n
-    gl_PointSize = 10.0; \n
-} \n
-"
+var vertShaderSource = "\
+void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 10.0;
+}"
 
-var fragShaderSource = " \
-void main() { \n
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n
-} \n
-"
+var fragShaderSource = "\
+void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}"
